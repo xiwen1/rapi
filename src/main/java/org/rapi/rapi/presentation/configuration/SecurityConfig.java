98aa2bc2b08b89955098bb4c95433aa87cdf6904
xiwen <1597083201@qq.com>
2024-12-14 11:36:25 +0800

    [upd] add cors configuration

--- src/main/java/org/rapi/rapi/presentation/configuration/SecurityConfig.java
+++ src/main/java/org/rapi/rapi/presentation/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.rapi.rapi.presentation.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
             .logout(AbstractHttpConfigurer::disable)
