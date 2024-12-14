b6dea3bfa409c6b17081d969fd85061dc9e80b61
xiwen <1597083201@qq.com>
2024-12-14 11:39:45 +0800

    [fix] error in invitation route

--- src/main/java/org/rapi/rapi/presentation/controller/InvitationController.java
+++ src/main/java/org/rapi/rapi/presentation/controller/InvitationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/invitations")
+@RequestMapping("/api/invitations")
 public class InvitationController {
 
     private final GetCurrentUserService getCurrentUserService;
